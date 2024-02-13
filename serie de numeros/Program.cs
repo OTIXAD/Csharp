0d538f24e0eadc3be23449abb7537b8f6a441f62
OTIXAD <158170216+OTIXAD@users.noreply.github.com>
2024-02-12 18:05:30 -0600

    Add files via upload

---
+++ serie de numeros/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serie_de_numeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear una serie de nuemros del 1 al 100.
+            int x = 1;
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            } while (x <=100);
+            Console.ReadKey();
+        }
+    }
+}
