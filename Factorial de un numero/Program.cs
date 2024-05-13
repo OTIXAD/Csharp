0d32eb0555980c12ecee7bcfae17791dbb411651
OTIXAD <158170216+OTIXAD@users.noreply.github.com>
2024-05-13 07:01:47 -0600

    Add files via upload

---
+++ Factorial de un numero/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial_de_un_numero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Factorial de un numero
+            long num = 0, factorial=0;
+            Console.WriteLine("A que numero deseas sacar su factorial?");
+            num= Convert.ToInt64(Console.ReadLine());
+
+            for(long x=num-1; x>0; x--)
+            {
+                factorial = num * x;
+                num= factorial;
+            }
+            Console.WriteLine(factorial);
+            Console.ReadKey();
+        }
+    }
+}
