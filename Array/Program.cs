61ffe58f4655dd7e944b2fb611c60a26e4e074e3
OTIXAD <158170216+OTIXAD@users.noreply.github.com>
2024-05-13 06:58:32 -0600

    Add files via upload

---
+++ Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //arrays
+            int longi = 0;
+            Console.WriteLine("Cuantas espacios se registraran?");
+            longi=Convert.ToInt32(Console.ReadLine());
+            int[] edades = new int[longi];//array dinamico
+            for(int posi=0; posi<edades.Length; posi++)
+            {
+                Console.WriteLine("Ingresa tu edad de la persona {0}", posi+1);
+                edades[posi] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int posi = 0; posi < edades.Length; posi++)
+            {
+                Console.Write(edades[posi]+", ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
