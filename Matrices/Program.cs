25e8ffc31b5e58f6bef90efbe5a294433383a200
OTIXAD <158170216+OTIXAD@users.noreply.github.com>
2024-05-13 07:04:40 -0600

    Add files via upload

---
+++ Matrices/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //arrays - matrices
+            int[,] matriz = new int[3, 3];
+
+            for(int x=0; x<4; x++)
+            {
+                for(int y=0; y<3; y++)
+                {
+                    Console.Write("{0},{1}| ", x, y);
+                }
+                Console.WriteLine();
+                Console.WriteLine("---------------");
+            }
+            Console.ReadKey();
+        }
+    }
+}
